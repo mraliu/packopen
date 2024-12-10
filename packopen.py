afa420c187e94d0e62c7d06503cb2f4e740a17a4
mraliu <teachmrliu@gmail.com>
2024-12-10 17:55:17 +0000

    with money

--- packopen.py
+++ packopen.py
@@ -1,14 +1,19 @@
 import random
 
+money =  100
+packcost = 5
 pokemons = ["Bulbasaur", "Charmander", "Squirtle", "Pidgeot", "Jigglypuff"]
 
 choice = ""
 
 while choice != "quit":
+    print("You have", 100, "credits")
+    print("A pack cost", packcost)
     choice = input("Do you want to open a pack? ")
 
     if choice == "yes":
+        money = money - packcost
         randomnumber = random.randint(0, len(pokemons)-1)
         pokemon = pokemons[randomnumber]
-
         print("You have got! ", pokemon)
+        print("You have ", money, "remaining.")
